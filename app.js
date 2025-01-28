ee7aee17ab8b13e550050dff47ccc7f14bb0b366
Tatiana Martinez <martineztati.3105@gmail.com>
2025-01-27 22:55:20 -0300

    Actualiza app

---
+++ app.js
@@ -0,0 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//declaraciones
+let listaAmigos = [];
+let listaHTML = [];
+
